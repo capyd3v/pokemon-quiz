1474b5f0f9f21556d12516fe8f751dcb944ed759
capyd3v <capydev@proton.me>
2025-11-01 09:45:13 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel
+from typing import List, Optional
 
+class PokemonResponse(BaseModel):
+    sprite: str
+    options: List[str]
+    correct_answer: str
+    types: List[str]
+
+class AnswerCheck(BaseModel):
+    selected_name: str
+    correct_answer: str
+    correct: bool
